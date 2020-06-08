82e183b476289e30b066a41bd6d5e515b16f6aea
Chris Aragon <aragonchristopherray@gmail.com>
2020-06-08 11:34:14 -0400

    changed note-helper to numbers

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,15 @@
 
 export const findFolder = (folders=[], id) =>
-  folders.find(folder => folder.id === Number(id))
+  folders.find(folder => Number(folder.id) === Number(id))
 
 export const findNote = (notes=[], id) =>
-  notes.find(note => note.id === Number(id))
+  notes.find(note => Number(note.id) === Number(id))
 
 export const getNotesForFolder = (notes=[], id) => (
   (!id)
     ? notes
-    : notes.filter(note => note.folder_id === Number(id))
+    : notes.filter(note => Number(note.folder_id) === Number(id))
 )
 
 export const countNotesForFolder = (notes=[], id) =>
-  notes.filter(note => note.folder_id === Number(id)).length
+  notes.filter(note => Number(note.folder_id) === Number(id)).length
